8f83d15cd02f6c5931a27ad6a0715fb8cc5ae95c
Wei Da <weida.lee.2017@sis.smu.edu.sg>
2021-01-19 22:42:06 +0800

    Update dashboard.php

--- app/Views/pages/dashboard.php
+++ app/Views/pages/dashboard.php
@@ -1,6 +1,6 @@
 <div class="container">
     <div class="row">
-        <div class="col-12">
+        <div class="col-12 text-center">
             <h1>Hello, <?= session()->get('username')?></h1>
         </div>
     </div>
